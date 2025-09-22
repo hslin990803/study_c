a3f9df949d1745053505f716929790be9577d60b
hslin990803 <hslin0803@gmail.com>
2025-09-22 19:41:18 +0900

    25.09.22 chapter 9 진행 중

--- study/study/chapter_9.c
+++ study/study/chapter_9.c
@@ -115,6 +115,35 @@
 * 세 개의 정수를 인자로 전달받아서 그 중 가장 큰 수를 반환하는 함수와 가장 작은 수를 반환함수를 정의해보자.
 * 그리고 이 함수들을 호출하는 적절한 main함수도 작성해보자.
 */
+//#include <stdio.h>
+//int MaxNum(int num1, int num2, int num3);
+//int MinNum(int num1, int num2, int num3);
+//
+//int main(void) {
+//	int num1, num2, num3;
+//
+//	printf("정수 3개 입력 : ");
+//	scanf("%d %d %d", &num1, &num2, &num3);
+//
+//	printf("가장 큰 수 : %d \n", MaxNum(num1, num2, num3));
+//	printf("가장 작은 수 : %d\n", MinNum(num1, num2, num3));
+//
+//	return 0;
+//}
+//
+//int MaxNum(int num1, int num2, int num3) {
+//	if (num1 > num2)
+//		return (num1 > num3) ? num1 : num3;
+//	else
+//		return (num2 > num3) ? num2 : num3;
+//}
+//
+//int MinNum(int num1, int num2, int num3) {
+//	if (num1 < num2)
+//		return (num1 < num3) ? num1 : num3;
+//	else
+//		return (num2 < num3) ? num2 : num3;
+//}
 
 /*
 * 문제 2
@@ -124,14 +153,108 @@
 * 참고로 섭씨와 화씨간 온도변환의 공식은 다음과 같다.
 * Fah = 1.8 X Cel + 32
 */
+//#include <stdio.h>
+//double CelToFah(double temp);
+//double FahToCel(double temp);
+//
+//int main(void) {
+//	int mode;
+//	double temp;
+//	
+//	printf("모드 입력 : \n 1. 섭씨 > 화씨 변환 2. 화씨 > 섭씨 변환");
+//	scanf("%d", &mode);
+//
+//	if (mode == 1) {
+//		printf("온도 입력 : ");
+//		scanf("%lf", &temp);
+//
+//		printf("변환 결과 : %lf\n", CelToFah(temp));
+//	}
+//	else if (mode == 2) {
+//		printf("온도 입력 : ");
+//		scanf("%lf", &temp);
+//
+//		printf("변환 결과 : %lf\n", FahToCel(temp));
+//	}
+//	
+//	return 0;
+//}
+//
+//double CelToFah(double temp) {
+//	return (1.8 * temp) + 32;
+//}
+//
+//double FahToCel(double temp) {
+//	return (temp - 32) / 1.8;
+//}
 
 /*
 * 문제 3
 * 인자로 전달된 수 만큼의 피보나치 수열을 출력하는 함수를 정의해보자.
 * 예를 들어서 프로그램 사용자가 5를 입력하면 0에서부터 시작해서 총 5개의 피보나치 수열을 출력해야 한다.
-* 참고로 피보나치 수여ㅑㄹ은 다음과 같다.
+* 참고로 피보나치 수열은 다음과 같다.
 * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ~
 * 이렇듯 피보나치 수열은 0과 1에서 시작한다.
 * 그리고 세 번째 이후의 수열부터는 이전의 두 값의 합으로 구성된다.
 * 즉, 3번째 수는 0과 1의 합으로 이뤄져서 1이 되고, 네 번째 수는 1과 1의 합으로 이뤄져서 2가된다.
 */
+//#include <stdio.h>
+//int Fibo(int num);
+//
+//int main(void) {
+//	int num;
+//
+//	printf("피보나치 수열 얼마나 출력하시겠습니까? \n");
+//	scanf("%d", &num);
+//
+//	Fibo(num);
+//
+//	return 0;
+//}
+//
+//int Fibo(int num) {
+//	int prevNum = 0;
+//	int nowNum = 1;
+//	int next;
+//
+//	printf("%d ", prevNum);
+//
+//	for (int i = 1; i < num; i++) {
+//		printf("%d ", nowNum);
+//		next = prevNum + nowNum;
+//		prevNum = nowNum;
+//		nowNum = next;
+//	}
+//	printf("\n");
+//}
+
+// P.222 LocalVariable.c
+//#include <stdio.h>
+//
+//int SimpleFuncOne(void) {
+//	int num = 10;		// 이후부터 SimpleFuncOne의 num 유효
+//	num++;
+//	printf("SimpleFuncOne num : %d \n", num);
+//
+//	return 0;		// SimpleFuncOne의 num이 유효한 마지막 문장
+//}
+//
+//int SimpleFuncTwo(void) {
+//	int num1 = 20;		// 이후부터 num1 유효
+//	int num2 = 30;		// 이후부터 num2 유효
+//
+//	num1++, num2--;
+//
+//	printf("num1 & num2 : %d %d \n", num1, num2);
+//
+//	return 0;		// num1, num2 유효한 마지막 문장
+//}
+//
+//int main(void) {
+//	int num = 17;		// 이후부터 main의 num 유효
+//	SimpleFuncOne();
+//	SimpleFuncTwo();
+//	printf("main num : %d \n", num);
+//
+//	return 0;		// main의 num이 유효한 마지막 문장
+//}
