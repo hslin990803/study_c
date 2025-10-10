5569fa021cf66476bfa4cfc1079c7246adca8c66
hslin990803 <hslin0803@gmail.com>
2025-10-10 19:22:16 +0900

    25.10.10 Chapter 14 진행중

--- study/study/chapter_14.c
+++ study/study/chapter_14.c
@@ -1,1 +1,19 @@
 // P.309 ~ 포인터와 함수에 대한 이해
+
+// P.312 ArrayParam.c
+//#include <stdio.h>
+//void ShowArayElem(int* param, int len) {
+//	int i;
+//	for (i = 0; i < len; i++)
+//		printf("%d ", param[i]);
+//	printf("\n");
+//}
+//
+//int main(void) {
+//	int arr1[3] = { 1,2,3 };
+//	int arr2[5] = { 4,5,6,7,8 };
+//	ShowArayElem(arr1, sizeof(arr1) / sizeof(int));
+//	ShowArayElem(arr2, sizeof(arr2) / sizeof(int));
+//
+//	return 0;
+//}
