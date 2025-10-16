2b416126e6819af82a7836a15ea51abfa13ea66a
hslin990803 <hslin0803@gmail.com>
2025-10-16 20:16:58 +0900

    25.10.16 Chapter 16 진행 중

--- study/study/chapter_16.c
+++ study/study/chapter_16.c
@@ -1,1 +1,13 @@
-// chapter_16
+// P.337 ~ 다차원 배열
+
+// P.339 TwoDimArraySize.c
+//#include <stdio.h>
+//int main(void) {
+//	int arr1[3][4];
+//	int arr2[7][9];
+//
+//	printf("세로3, 가로4 : %d \n", sizeof(arr1));
+//	printf("세로7, 가로9 : %d \n", sizeof(arr2));
+//
+//	return 0;
+//}
