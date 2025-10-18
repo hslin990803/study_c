b34894dec2128781bda5d7d8296903a079aa45c4
hslin990803 <hslin0803@gmail.com>
2025-10-18 13:23:38 +0900

    25.10.18 Chapter 16 진행 중

--- study/study/chapter_16.c
+++ study/study/chapter_16.c
@@ -36,3 +36,14 @@
 //
 //	return 0;
 //}
+
+// P.344 TwoDimArrayAddr.c
+//#include <stdio.h>
+//int main(void) {
+//	int arr[3][2];
+//	int i, j;
+//	for (i = 0; i < 3; i++)
+//		for (j = 0; j < 2; j++)
+//			printf("%p \n", &arr[i][j]);
+//	return 0;
+//}
