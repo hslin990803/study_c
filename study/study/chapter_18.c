cd3ecec9dd804644dfb40ed54f48c3fc20bd32de
hslin990803 <hslin0803@gmail.com>
2025-10-25 11:42:09 +0900

    25.10.25 Chapter 18 진행중

--- study/study/chapter_18.c
+++ study/study/chapter_18.c
@@ -21,3 +21,19 @@
 //
 //	return 0;
 //}
+
+// P.376 2DArrPointerOp.c
+//#include <stdio.h>
+//int main(void) {
+//	int arr1[3][2];
+//	int arr2[2][3];
+//
+//	printf("arr1: %p \n", arr1);
+//	printf("arr1+1: %p \n", arr1 + 1);
+//	printf("arr1+2: %p \n\n", arr1 + 2);
+//
+//	printf("arr2: %p \n", arr2);
+//	printf("arr2+1: %p \n", arr2 + 1);
+//
+//	return 0;
+// }
