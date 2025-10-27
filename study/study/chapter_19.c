ef58dc8734c01971f1974fb2cef9daf55ebe138f
hslin990803 <hslin0803@gmail.com>
2025-10-27 23:47:46 +0900

    25.10.27 Chapter 19 진행중

--- study/study/chapter_19.c
+++ study/study/chapter_19.c
@@ -1,1 +1,25 @@
 // P.391 ~ 함수 포인터와 void 포인터
+
+// P.394 FunctionPointer.c
+//#include <stdio.h>
+//void SimpleAdder(int n1, int n2) {
+//	printf("%d + %d = %d \n", n1, n2, n1 + n2);
+//}
+//
+//void ShowString(char* str) {
+//	printf("%s \n", str);
+//}
+//
+//int main(void) {
+//	char* str = "Function Pointer";
+//	int num1 = 10, num2 = 20;
+//
+//	void (*fptr1)(int, int) = SimpleAdder;
+//	void (*fptr2)(char*) = ShowString;
+//
+//	/* 함수 포인터 변수에 의한 호출 */
+//	fptr1(num1, num2);
+//	fptr2(str);
+//
+//	return 0;
+//}
