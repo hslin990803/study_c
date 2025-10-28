258b689cddaa22fe6bf7b1bdc570c2b239bdb4ea
hslin990803 <hslin0803@gmail.com>
2025-10-28 22:03:12 +0900

    25.10.28 Chapter 19 완료

--- study/study/chapter_19.c
+++ study/study/chapter_19.c
@@ -23,3 +23,107 @@
 //
 //	return 0;
 //}
+
+// P.395 UsefulFunctionPointer.c
+//#include <stdio.h>
+//int WhoIsFirst(int age1, int age2, int (*cmp)(int n1, int n2)) {
+//	return cmp(age1, age2);
+//}
+//
+//int OlderFirst(int age1, int age2) {
+//	if (age1 > age2)
+//		return age1;
+//	else if (age1 < age2)
+//		return age2;
+//	else
+//		return 0;
+//}
+//
+//int YongerFirst(int age1, int age2) {
+//	if (age1 < age2)
+//		return age1;
+//	else if (age1 > age2)
+//		return age2;
+//	else
+//		return 0;
+//}
+//
+//int main(void) {
+//	int age1 = 20;
+//	int age2 = 30;
+//	int first;
+//
+//	printf("입장순서 1 \n");
+//	first = WhoIsFirst(age1, age2, OlderFirst);
+//	printf("%d세와 %d세 중 %d세가 먼저 입장! \n\n", age1, age2, first);
+//
+//	printf("입장순서 2 \n");
+//	first = WhoIsFirst(age1, age2, YongerFirst);
+//	printf("%d세와 %d세 중 %d세가 먼저 입장! \n\n", age1, age2, first);
+//
+//	return 0;
+//}
+
+// P.397 VoidTypePointer.c
+//#include <stdio.h>
+//void SoSimpleFunc(void) {
+//	printf("I'm so simple");
+//}
+//
+//int main(void) {
+//	int num = 20;
+//	void* ptr;
+//
+//	ptr = &num;		// 변수 num의 주소 값 저장
+//	printf("%p \n", ptr);
+//
+//	ptr = SoSimpleFunc;		// 함수 SoSimpleFunc의 주소 값 저장
+//	printf("%p \n", ptr);
+//
+//	return 0;
+//}
+
+// P.399 ArgcArgv.c
+//#include <stdio.h>
+//int main(int argc, char* argv[]) {
+//	int i = 0;
+//	printf("전달된 문자열의 수 : %d \n", argc);
+//
+//	for (i = 0; i < argc; i++)
+//		printf("%d번째 문자열 : %s \n", i + 1, argv[i]);
+//
+//	return 0;
+//}
+
+// P.401 ArgvParamType.c
+//#include <stdio.h>
+//void ShowAllString(int argc, char* argv[]) {
+//	int i;
+//	for (i = 0; i < argc; i++)
+//		printf("%s \n", argv[i]);
+//}
+//
+//int main(void) {
+//	char* str[3] = {
+//		"C Programming",
+//		"c++ Programming",
+//		"JAVA Programming"
+//	};
+//
+//	ShowAllString(3, str);
+//	return 0;
+//}
+
+// P.403 ArgvEndNULL.c
+//#include <stdio.h>
+//int main(int argc, char* argv[]) {
+//	int i = 0;
+//	printf("전달된 문자열의 수 : %d \n", argc);
+//
+//	while (argv[i] != NULL) {
+//		printf("%d번째 문자열 : %s \n", i + 1, argv[i]);
+//		i++;
+//	}
+//
+//	return 0;
+//}
