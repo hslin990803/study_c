c9e6fbc302405bff0a0af208cb6b11c3f218f9b2
hslin990803 <hslin0803@gmail.com>
2025-11-03 22:34:01 +0900

    25.11.03 Chapter 21 진행중

--- study/study/chapter_21.c
+++ study/study/chapter_21.c
@@ -54,3 +54,18 @@
 //
 //	return 0;
 //}
+
+// P.426 ReadString.c
+//#include <stdio.h>
+//int main(void)
+//{
+//	char str[7];
+//	int i;
+//
+//	for (i = 0; i < 3; i++) {
+//		fgets(str, sizeof(str), stdin);
+//		printf("Read %d : %s \n", i + 1, str);
+//	}
+//
+//	return 0;
+//}
