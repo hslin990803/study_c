596a3fd6bb84e6798a4541cbe0cc1f550ae57333
hslin990803 <hslin0803@gmail.com>
2025-11-04 20:46:39 +0900

    25.11.04 Chapter 21 진행 중

--- study/study/chapter_21.c
+++ study/study/chapter_21.c
@@ -69,3 +69,21 @@
 //
 //	return 0;
 //}
+
+// P.431 NeedInputBufFlush.c
+//#include <stdio.h>
+//int main(void) {
+//	char perID[7];
+//	char name[10];
+//
+//	fputs("주민번호 앞 6자리 입력 : ", stdout);
+//	fgets(perID, sizeof(perID), stdin);
+//
+//	fputs("이름 입력 : ", stdout);
+//	fgets(name, sizeof(name), stdin);
+//
+//	fputs("주민번호 : %s \n", perID);
+//	printf("이름 : %s \n", name);
+//
+//	return 0;
+//}
