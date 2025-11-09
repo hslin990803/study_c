ce30fc72213210492732f5b7949603d7d654c309
hslin990803 <hslin0803@gmail.com>
2025-11-09 22:25:37 +0900

    25.11.09 Chapter 21 진행중

--- study/study/chapter_21.c
+++ study/study/chapter_21.c
@@ -158,3 +158,24 @@
 //
 //	return 0;
 //}
+
+// P.440 StringConcatCase.c
+//#include <stdio.h>
+//#include <string.h>
+//int main(void) {
+//	char str1[20] = "First~";
+//	char str2[20] = "Second";
+//
+//	char str3[20] = "Simple num : ";
+//	char str4[20] = "1234567890";
+//
+//	/* case 1 */
+//	strcat(str1, str2);
+//	puts(str1);
+//
+//	/* case 2 */
+//	strncat(str3, str4, 7);
+//	puts(str3);
+//
+//	return 0;
+//}
