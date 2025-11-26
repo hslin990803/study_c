4b58cca344269b76b62d1ed4d227c1da11c94677
hslin990803 <hslin0803@gmail.com>
2025-11-26 23:52:55 +0900

    25.11.26 Chapter 23 진행 중

--- study/study/chapter_23.c
+++ study/study/chapter_23.c
@@ -51,3 +51,30 @@
 //
 //	return 0;
 //}
+
+// P.478 StructValAndFunction.c
+//#include <stdio.h>
+//
+//typedef struct point {
+//	int xpos;
+//	int ypos;
+//} Point;
+//
+//void ShowPosition(Point pos) {
+//	printf("[%d, %d] \n", pos.xpos, pos.ypos);
+//}
+//
+//Point GetCurrentPostion(void) {
+//	Point cen;
+//	printf("Input current pos : ");
+//	scanf("%d %d", &cen.xpos, &cen.ypos);
+//
+//	return cen;
+//}
+//
+//int main(void) {
+//	Point curPos = GetCurrentPostion();
+//	ShowPosition(curPos);
+//
+//	return 0;
+//}
