b7d1e9f3d816458e85dfa9dfd79964ef8f0ef6b8
hslin990803 <hslin0803@gmail.com>
2025-12-02 19:07:07 +0900

    25.12.02 Chapter 23 진행 중

--- study/study/chapter_23.c
+++ study/study/chapter_23.c
@@ -202,3 +202,40 @@
 * 이 두 구조체 변수를 대상으로, 혹은 두 구조체 변수의 주소 값을 대상으로 SwapPoint라는 이름의 함수를 호출하면
 * pos1의 xpos, ypos에는 각각 5와 7이, 그리고 pos2의 xpos, ypos에는 각각 2와 4가 저장되어야 한다.
 */
+
+// P.485 StructImportant.c
+//#include <stdio.h>
+//typedef struct student {
+//	char name[20];		// 학생이름
+//	char stdnum[20];	// 학생 고유번호
+//	char school[20];	// 학교 이름
+//	char major[20];		// 선택 전공
+//	int year;			// 학년
+//} Student;
+//
+//void ShowStudentInfo(Student* sptr) {
+//	printf("학생 이름 : %s \n", sptr->name);
+//	printf("학생 고유번호 : %s \n", sptr->stdnum);
+//	printf("학교 이름 : %s \n", sptr->school);
+//	printf("선택 전공 : %s \n", sptr->major);
+//	printf("학년 : %d \n", sptr->year);
+//}
+//
+//int main(void) {
+//	Student arr[7];
+//	int i;
+//
+//	for (i = 0; i < 7; i++) {
+//		printf("이름 : "); scanf("%s", arr[i].name);
+//		printf("번호 : "); scanf("%s", arr[i].stdnum);
+//		printf("학교 : "); scanf("%s", arr[i].school);
+//		printf("전공 : "); scanf("%s", arr[i].major);
+//		printf("학년 : "); scanf("%d", &arr[i].year);
+//	}
+//
+//	for (i = 0; i < 7; i++) {
+//		ShowStudentInfo(&arr[i]);
+//	}
+//
+//	return 0;
+//}
