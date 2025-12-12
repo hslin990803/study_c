c221a0ea8947739f8971f22e06f0aa55c23a07b5
hslin990803 <hslin0803@gmail.com>
2025-12-12 15:57:01 +0900

    25.12.12 Chapter 24 진행 중

--- study/study/chapter_24.c
+++ study/study/chapter_24.c
@@ -1,1 +1,19 @@
 // P.501 ~ 파일 입출력
+
+// P.505 FirstFileWrite.c
+//#include <stdio.h>
+//
+//int main(void) {
+//	FILE* fp = fopen("data.txt", "wt");
+//	if (fp == NULL) {
+//		puts("파일오픈 실패!");
+//		return -1;	//	비정상적 종료를 의미하기 위해서 -1을 반환
+//	}
+//
+//	fputc('A', fp);
+//	fputc('B', fp);
+//	fputc('C', fp);
+//	fclose(fp);		// 스트림의 종료
+//
+//	return 0;
+//}
