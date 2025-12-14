ca8852a812fa30ef9140060e50ecc3fab422fd42
hslin990803 <hslin0803@gmail.com>
2025-12-14 21:02:28 +0900

    25.12.14 Chapter 24 진행 중

--- study/study/chapter_24.c
+++ study/study/chapter_24.c
@@ -36,3 +36,23 @@
 //
 //	return 0;
 //}
+
+// P.515 TextDataFileWrite.c
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	FILE* fp = fopen("simple.txt", "wt");
+//	if (fp == NULL) {
+//		puts("파일오픈 실패!");
+//		return -1;
+//	}
+//
+//	fputc('A', fp);
+//	fputc('B', fp);
+//	fputc("My name is Hong \n", fp);
+//	fputc("Your name is Yoon \n", fp);
+//	fclose(fp);
+//
+//	return 0;
+//}
