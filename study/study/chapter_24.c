190d68f0055ab750bb627cfd153132aa51982219
hslin990803 <hslin0803@gmail.com>
2025-12-15 22:41:17 +0900

    25.12.15 Chapter 25 진행 중

--- study/study/chapter_24.c
+++ study/study/chapter_24.c
@@ -56,3 +56,28 @@
 //
 //	return 0;
 //}
+
+// P.515 TextDataFileRead.c
+//#include <stdio.h>
+//int main(void) {
+//	char str[30];
+//	int ch;
+//	FILE* fp = fopen("simple.txt", "rt");
+//	if (fp == NULL) {
+//		puts("파일오픈 실패!");
+//		return -1;
+//	}
+//
+//	ch = fgetc(fp);
+//	printf("%c \n", ch);
+//	ch = fgetc(fp);
+//	printf("%c \n", ch);
+//
+//	fgets(str, sizeof(str), fp);
+//	printf("%s", str);
+//	fgets(str, sizeof(str), fp);
+//	printf("%s", str);
+//
+//	fclose(fp);
+//	return 0;
+//}
