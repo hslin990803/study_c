2601c5130be9db06b67943796aca88b90aeaeb1e
hslin990803 <hslin0803@gmail.com>
2025-12-28 23:20:07 +0900

    25.12.28 Chapter 24 진행 중

--- study/study/chapter_24.c
+++ study/study/chapter_24.c
@@ -218,3 +218,24 @@
 //
 //	return 0;
 //}
+
+// P.525 ComplexFileRead.c
+//#include <stdio.h>
+//int main(void) {
+//	char name[10];
+//	char sex;
+//	int age;
+//
+//	FILE* fp = fopen("freind.txt", "rt");
+//	int ret;
+//
+//	while (1) {
+//		ret = fscanf(fp, "%s %c %d", name, &sex, &age);
+//		if (ret == EOF)
+//			break;
+//		printf("%s %c %d \n", name, sex, age);
+//	}
+//	fclose(fp);
+//	
+//	return 0;
+//}
