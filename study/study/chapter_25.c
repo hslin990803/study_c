75c1970ecc93a6f715aac0cdf8b7e7f21503f49d
hslin990803 <hslin0803@gmail.com>
2026-01-06 20:20:00 +0900

    26.01.06 Chapter 25 진행 중

--- study/study/chapter_25.c
+++ study/study/chapter_25.c
@@ -42,3 +42,30 @@
 //
 //	return 0;
 //}
+
+// P.543 ReadStringFault2.c
+//#include <stdio.h>
+//char name[30];
+//
+//char* ReadUserName(void)
+//{
+//	printf("What's your anme? ");
+//	gets(name);
+//
+//	return name;
+//}
+//
+//int main(void)
+//{
+//	char* name1;
+//	char* name2;
+//	name1 = ReadUserName();
+//	printf("name1 : %s \n", name1);
+//	name2 = ReadUserName();
+//	printf("name2 : %s \n", name2);
+//
+//	printf("name1 : %s \n", name1);
+//	printf("name2 : %s \n", name2);
+//
+//	return 0;
+//}
