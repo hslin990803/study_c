66f80200f374fa3a13a12922d200531c1f153d22
hslin990803 <hslin0803@gmail.com>
2026-01-13 15:20:14 +0900

    26.01.13 Chapter 26 진행 중

--- study/study/chapter_26.c
+++ study/study/chapter_26.c
@@ -1,1 +1,16 @@
-// P.555 ~ 매크로와 선행처리기(Preprocessor)
+// P.555 ~ 매크로와 선행처리기(Preprocessor) 
+
+// P.559 MacroConst.c
+//#include <stdio.h>	
+//
+//#define NAME	"홍길동"
+//#define AGE		24
+//#define PRINT_ADDR	puts("주소 : 경기도 용인시 \n");
+//
+//int main(void) {
+//	printf("이름 : %s \n", NAME);
+//	printf("나이 : %d \n", AGE);
+//	PRINT_ADDR;
+//
+//	return 0;
+//}
