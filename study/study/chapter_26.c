d9860d6139524740e36671b33ef6092c64e36601
hslin990803 <hslin0803@gmail.com>
2026-01-15 19:49:52 +0900

    26.01.15 Chapter 26 진행 중

--- study/study/chapter_26.c
+++ study/study/chapter_26.c
@@ -31,3 +31,16 @@
 //	printf("Square of 3-2 : %d \n", SQUARE(3 + 2));
 //	return 0;
 //}
+
+// P.563 MacroUseMacro.c
+//#include <stdio.h>
+//#define PI 3.14
+//#define PRODUCT(X, Y) ((X) * (Y))
+//#define CIRCLE_AREA(R) (PRODUCT((R), (R)) * PI)
+//
+//int main(void) {
+//	double rad = 2.1;
+//	printf("반지름 : %g인 원의 넓이 : %g \n", rad, CIRCLE_AREA(rad));
+//
+//	return 0;
+//}
