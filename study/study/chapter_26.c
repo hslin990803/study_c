c493eb9f133359e94902bd47353b3453300c68ca
hslin990803 <hslin0803@gmail.com>
2026-01-16 21:14:16 +0900

    26.01.16 Chapter 26 진행 중

--- study/study/chapter_26.c
+++ study/study/chapter_26.c
@@ -44,3 +44,14 @@
 //
 //	return 0;
 //}
+
+// P.565 MacroFunctionWeakness.c
+//#include <stdio.h>
+//#define DIFF_ABS(X, Y) ((X)>(Y) ? (X)-(Y) : (Y)-(X))
+//
+//int main(void) {
+//	printf("두 값의 차 : %d \n", DIFF_ABS(5, 7));
+//	printf("두 값의 차 : %d \n", DIFF_ABS(1.8, -1.4));
+//
+//	return 0;
+//}
