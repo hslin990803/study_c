09079c26e10f3e9b996c18c5b7663f3908ee614c
hslin990803 <hslin0803@gmail.com>
2026-01-18 20:02:03 +0900

    26.01.18 Chapter 26 진행 중

--- study/study/chapter_26.c
+++ study/study/chapter_26.c
@@ -75,3 +75,23 @@
 //#endif
 //		return 0;
 //}
+
+// P.568 ifdef_endif.c
+//#include <stdio.h>
+//// #define ADD	1
+//#define MIN	0
+//
+//int main(void) {
+//	int num1, num2;
+//	printf("두 개의 정수 입력 : ");
+//	scanf("%d %d", &num1, &num2);
+//
+//#ifdef ADD	// ADD가 참이라면
+//	printf("%d + %d = %d \n", num1, num2, num1 + num2);
+//#endif
+//
+//#ifdef MIN	// MIN이 '참'이라면
+//	printf("%d - %d = %d \n", num1, num2, num1 - num2);
+//#endif
+//	return 0;
+//}
