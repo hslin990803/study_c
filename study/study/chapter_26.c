0674953baa041af16936693b0d03f2ebd67b52e4
hslin990803 <hslin0803@gmail.com>
2026-01-22 20:25:51 +0900

    26.01.22 Chapter 26 진행 중

--- study/study/chapter_26.c
+++ study/study/chapter_26.c
@@ -136,3 +136,15 @@
 //
 //	return 0;
 //}
+
+// P.574 UnivStdNum.c
+//#include <stdio.h>
+////#define STNUM(Y, S, P)	YSP
+////#define STNUM(Y, S, P)	Y S P
+//#define STNUM(Y, S, P)	Y((Y)*100000+(S)*1000+(p))
+//
+//int main(void) {
+//	printf("학번 : %d \n", STNUM(10, 65, 175));
+//	printf("학번 : %d \n", STNUM(10, 65, 075));
+//	return 0;
+//}
