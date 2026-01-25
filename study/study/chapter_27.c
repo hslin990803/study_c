78cb333b3cb78d5d88fd8a4bf42b940a4b2e6c6c
hslin990803 <hslin0803@gmail.com>
2026-01-25 22:56:22 +0900

    26.01.25 Chapter 27 진행 중

--- study/study/chapter_27.c
+++ study/study/chapter_27.c
@@ -1,1 +1,23 @@
 // P.580 ~ 파일의 분할과 헤더파일의 디자인
+
+// P.580 SimpleOneFile.c
+//#include <stdio.h>
+//int num = 0;
+//
+//void Increment(void) {
+//	num++;
+//}
+//
+//int GetNum(void) {
+//	return num;
+//}
+//
+//int main(void) {
+//	printf("num : %d \n", GetNum());
+//	Increment();
+//	printf("num : %d \n", GetNum());
+//	Increment();
+//	printf("num : %d \n", GetNum());
+//
+//	return 0;
+//}
