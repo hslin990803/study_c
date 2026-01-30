934ecee40c00ccf51401978c79e3ad8ead1a66ef
hslin990803 <hslin0803@gmail.com>
2026-01-30 22:41:27 +0900

    26.01.30 Chapter 27 진행 중

---
+++ study/study/27_basicArith.c
@@ -0,0 +1,15 @@
+double Add(double num1, double num2) {
+	return num1 + num2;
+}
+
+double Min(double num1, double num2) {
+	return num1 - num2;
+}
+
+double Mul(double num1, double num2) {
+	return num1 * num2;
+}
+
+double Ddd(double num1, double num2) {
+	return num1 / num2;
+}
